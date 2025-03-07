b5de756c27b70e64fb7685682ea2bfaf79d8c735
Oleg Baranov <xssnick8@gmail.com>
2025-03-07 16:31:39 +0400

    Tests info

--- tests/PaymentChannel.spec.ts
+++ tests/PaymentChannel.spec.ts
@@ -5,35 +5,7 @@ import '@ton/test-utils';
 import { compile } from '@ton/blueprint';
 
 describe('PaymentChannel', () => {
-    let code: Cell;
-
-    beforeAll(async () => {
-        code = await compile('PaymentChannel');
-    });
-
-    let blockchain: Blockchain;
-    let deployer: SandboxContract<TreasuryContract>;
-    let paymentChannel: SandboxContract<PaymentChannel>;
-
-    beforeEach(async () => {
-        blockchain = await Blockchain.create();
-
-        paymentChannel = blockchain.openContract(PaymentChannel.createFromConfig({}, code));
-
-        deployer = await blockchain.treasury('deployer');
-
-        const deployResult = await paymentChannel.sendDeploy(deployer.getSender(), toNano('0.05'));
-
-        expect(deployResult.transactions).toHaveTransaction({
-            from: deployer.address,
-            to: paymentChannel.address,
-            deploy: true,
-            success: true,
-        });
-    });
-
-    it('should deploy', async () => {
-        // the check is done inside beforeEach
-        // blockchain and paymentChannel are ready to use
+    it('nop', async () => {
+       console.log("Tests are made in golang payment-network repository, using testnet, because of complexity")
     });
 });
